37baffe73b5b150ccd4fc00138f6feecf6c089f7
doannhu <ndodoan@gmail.com>
2024-05-06 00:40:15 +1000

    Add Endpoint Filter

---
+++ MerchantsAPI_p2/Endpoint Filters/MerchantLockedFilter.cs
@@ -0,0 +1,45 @@
+﻿namespace MerchantsAPI_p2.Endpoint_Filters
+{
+    public class MerchantLockedFilter: IEndpointFilter
+    {
+        private Guid _id;
+
+        public MerchantLockedFilter(Guid id)
+        {
+            _id = id;
+        }
+        public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context,
+                                              EndpointFilterDelegate next)
+        {
+            Guid merchantId;
+            if (context.HttpContext.Request.Method == "PUT")
+            {
+                merchantId = context.GetArgument<Guid>(3); // This is order of arguments in PutMerchantUpdatePaymentAsync()
+                                                           // 0: Db context, 1: Mapper, 2: MerchantUpdatePayment,
+                                                           // 3: Guid
+            }
+            else if (context.HttpContext.Request.Method == "DELETE")
+            {
+                merchantId= context.GetArgument<Guid>(1); // This is order of arguments in DeleteMerchantAsync() 0: Db context, 1: Guid
+            }
+            else
+            {
+                throw new NotSupportedException("This filter is not supported for this method");
+            }
+
+            // var blockedId = new Guid("91d76269-0959-44ea-a1ba-5f9e3c83272d");
+            var blockedId = _id;
+            if (merchantId == blockedId)
+            {
+                return TypedResults.Problem(new()
+                {
+                    Status = 400,
+                    Title = "Not allow to be updated or removed",
+                    Detail = "Default merchant"
+                });
+            }
+            var result = await next.Invoke(context);
+            return result;
+        }
+    }
+}
--- MerchantsAPI_p2/Extensions/EndPointRouteBuilderExtensions.cs
+++ MerchantsAPI_p2/Extensions/EndPointRouteBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using MerchantsAPI_p2.EndpointHandlers;
+﻿using MerchantsAPI_p2.Endpoint_Filters;
+using MerchantsAPI_p2.EndpointHandlers;
 
 namespace MerchantsAPI_p2.Extensions
 {
@@ -8,15 +9,20 @@ namespace MerchantsAPI_p2.Extensions
         {
             var merchantsEndpoints = endpointRouteBuilder.MapGroup("/merchants/v1");
 
+
             merchantsEndpoints.MapGet("", MerchantHandler.GetMerchantsAsync);
 
             merchantsEndpoints.MapGet("/{merchant_unique_id:guid}", MerchantHandler.GetAMerchantAsync).WithName("GetMerchant");
 
             merchantsEndpoints.MapPost("", MerchantHandler.PostMerchantAsync);
 
-            merchantsEndpoints.MapPut("/{merchant_id:guid}/payment-method", MerchantHandler.PutMerchantUpdatePaymentAsync);
+            merchantsEndpoints.MapPut("/{merchant_id:guid}/payment-method", MerchantHandler.PutMerchantUpdatePaymentAsync).
+                                    AddEndpointFilter(new MerchantLockedFilter(new Guid("91d76269-0959-44ea-a1ba-5f9e3c83272d"))).
+                                    AddEndpointFilter(new MerchantLockedFilter(new Guid("f6767a42-955e-403c-bd38-520f5f7af6ec")));
 
-            merchantsEndpoints.MapDelete("/{merchant_id:guid}", MerchantHandler.DeleteMerchantAsync);
+            merchantsEndpoints.MapDelete("/{merchant_id:guid}", MerchantHandler.DeleteMerchantAsync).
+                                    AddEndpointFilter(new MerchantLockedFilter(new Guid("91d76269-0959-44ea-a1ba-5f9e3c83272d"))).
+                                    AddEndpointFilter(new MerchantLockedFilter(new Guid("f6767a42-955e-403c-bd38-520f5f7af6ec")));
 
             merchantsEndpoints.MapDelete("", () => {
                 throw new NotImplementedException();    
